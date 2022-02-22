2d0e65a6b987e3ac52c34acb705cd7eaca4b6464
Sagir Yusuf Muhammad <sagware@gmail.com>
2022-02-22 11:53:38 +0000

    I edited the title

--- about.blade.php
+++ about.blade.php
@@ -5,7 +5,7 @@
 <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
-	<title>About the Materials and Manufacturing in Healthcare Innovation Network</title>
+	<title>About the Materials and Manufacturing in Healthcare Innovation</title>
 	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0,  name="viewport" />
 	<meta content="" name="description" />
 	<meta content="" name="author" />
