2425df94573da046ac77bd2c1c9946b1040e44ba
Charnettcc <charnett.chau@ucl.ac.uk>
2022-02-24 13:50:53 +0000

    MMHIN 103

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
-	<title>About Materials and Manufacturing in Healthcare Innovation Network</title>
+	<title>About the Materials and Manufacturing in Healthcare Innovation Network</title>
 	<meta content="width=device-width" name="viewport">
 	<meta content="" name="description" />
 	<meta content="" name="author" />
